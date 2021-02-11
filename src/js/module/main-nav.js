246ddf6ecdfdc5a6bb261574cc8b1b9cc2707004
Max <maximkasupp@gmail.com>
2021-02-11 11:29:04 +0200

    1

---
+++ src/js/module/main-nav.js
@@ -0,0 +1,11 @@
+document.addEventListener('DOMContentLoaded', function() {
+
+  var Nav = new hcOffcanvasNav('#main-nav', {
+    disableAt: 1024,
+    customToggle: '.toggle',
+    navTitle: 'All Categories',
+    levelTitles: true,
+    levelTitleAsBack: true
+  });
+
+});
